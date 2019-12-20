9b6344c4ef88afc977af4ed39f6c29c583f7f43a
JadeAnarchy499 <jadesprout499@gmail.com>
2019-12-20 14:19:10 +0100

    no message

--- source/repos/Section_02/BullCowGame/FBullCowGame.h
+++ source/repos/Section_02/BullCowGame/FBullCowGame.h
@@ -1,20 +1,52 @@
+/* The game logic (no view code or direct user interaction)
+The game is a simple guess the word game based on Mastermind
+*/
+
 #pragma once
 #include <string>
 
-class FBullCowGame {
+// to make syntax Unreal friendly
+using FString = std::string;
+using int32 = int;
+
+struct FBullCowCount
+{
+	int32 Bulls = 0;
+	int32 Cows = 0;
+};
+
+enum class EGuessStatus
+{
+	Invalid_Status,
+	OK,
+	Not_Isogram,
+	Wrong_Length,
+	Not_Lowercase
+};
+
+
+class FBullCowGame
+{
 public:
 	FBullCowGame(); // constructor
 
-	int GetMaxTries() const;
-	int GetCurrentTry() const;
+	int32 GetMaxTries() const;
+	int32 GetCurrentTry() const;
+	int32 GetHiddenWordLength() const;
 	bool IsGameWon() const;
+	EGuessStatus CheckGuessValidity(FString) const;
 
-	void Reset(); // TODO make a more rich return value
-	bool CheckGuessValidity(std::string); // TODO make a more rich return value
-	// provide a method for counting bulls & cows, and increasing try #
+	void Reset();
+	FBullCowCount SubmitValidGuess(FString);
 
+
+	// ^^ Please try and ignore this and focus on the interface above ^^
 private:
 	// see constructor for initialisation
-	int MyCurrentTry;
-	int MyMaxTries;
+	int32 MyCurrentTry;
+	FString MyHiddenWord;
+	bool bGameIsWon;
+
+	bool IsIsogram(FString) const;
+	bool IsLowercase(FString) const;
 };
